214b9dca16f51397d2f9034d916c8d2ba49f5c6f
unknown <fajarrizki961@gmail.com>
2023-12-16 23:17:09 +0700

    login

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,18 @@
-export default function Login() {
+'use client';
+
+import React from "react";
+import FormLogin from "./formLogin";
+import { useState } from "react";
+const Login = () => {
+    const [userData, setUserData] = useState(null);
+    const handleLogin = (data) => {
+        setUserData(data);
+        window.location.href ="./dashboard"
+    }
     return (
-      <>
-        Ini tampilan Login
-      </>
+        <>
+            <FormLogin/>
+        </>
     )
-  }
+}
+export default Login
